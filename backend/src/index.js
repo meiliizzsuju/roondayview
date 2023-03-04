c4bf9593783b70a905665c07326ac6e24d2c635a
ASIANBOI11 <tommy.truong962@gmail.com>
2023-03-04 23:20:15 +1100

    cors fix test

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,3 +8,37 @@ app.listen(PORT, () => {
         console.log("Database connected")
     })
 })
+
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+require("dotenv").config()
+
+const foodblogRouter = require("./controllers/foodblogs/foodblogRoutes")
+const userRouter = require("./controllers/users/userRoutes")
+const contactRouter = require("./controllers/contact/contactRoutes")
+
+const app = express()
+
+app.use(helmet())
+
+app.use(express.json())
+
+const corsOption = {
+    orgin: ["http:localhost:3000", "https://legendary-arithmetic-c1ee45.netlify.app"], 
+    optionsSuccessStatus: 200
+}
+
+app.get("/", (request, response) => {
+    response.json({
+        data: "Data Send"
+    })
+})
+
+app.use(cors(corsOption))
+
+const PORT = process.env.PORT || 5000
+
+app.use("/foodblogs", foodblogRouter)
+app.use("/users", userRouter)
+app.use("/contact", contactRouter)
