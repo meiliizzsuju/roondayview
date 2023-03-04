12d9348816e0abee577b01081a3f92d88568d34d
ASIANBOI11 <tommy.truong962@gmail.com>
2023-03-04 22:38:53 +1100

    trying to fix cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,14 @@ app.get("/", (request, response) => {
     })
 })
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
+
 const corsOption = {
     orgin: ["http:localhost:3000", "https://legendary-arithmetic-c1ee45.netlify.app"], 
     optionsSuccessStatus: 200
