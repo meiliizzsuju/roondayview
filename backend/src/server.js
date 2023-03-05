39d8f5e2e587e222d662f7d15a04bed4cdd63982
ASIANBOI11 <tommy.truong962@gmail.com>
2023-03-05 18:39:55 +1100

    comments 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const helmet = require("helmet")
 const dotenv = require("dotenv");
 
+// All the controller router
 const foodblogRouter = require("./controllers/foodblogs/foodblogRoutes")
 const userRouter = require("./controllers/users/userRoutes")
 const contactRouter = require("./controllers/contact/contactRoutes")
@@ -16,14 +17,14 @@ app.use(helmet())
 
 app.use(express.json())
 
-
-
+// Test if the database works
 app.get("/", (request, response) => {
     response.json({
         data: "Data Send"
     })
 })
 
+// This will link the frontend
 const corsOption = {
     origin: ["http://localhost:3000", "https://legendary-arithmetic-c1ee45.netlify.app"], 
     optionsSuccessStatus: 200
@@ -33,6 +34,7 @@ app.use(cors(corsOption))
 
 const PORT = process.env.PORT || 5000
 
+// Use the routers
 app.use("/foodblogs", foodblogRouter)
 app.use("/users", userRouter)
 app.use("/contact", contactRouter)
